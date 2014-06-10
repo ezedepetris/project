f8e789c119a38b92a232f49fcf900a0b0666c260
ezequiel <ezedepetris@gmai.com>
2014-06-10 14:22:05 -0300

    add list to tree and insert a elemen in a tree

--- proyecto.hs
+++ proyecto.hs
@@ -1,4 +1,4 @@
-data (Ord a) => BinTree a = Null | Node (BinTree a) a (BinTree a)
+data (Ord a) => BinTree a = Null | Node (BinTree a) a (BinTree a) deriving Show
 
 ------- RETURN HE ORIGIN OF THE MAIN TREE ------
 raiz :: (Ord a)  => BinTree a -> a
@@ -14,7 +14,7 @@ treeToList ( Node i x d ) =  treeToList ( i ) ++ [x] ++ treeToList ( d )
 ------ VERIFY IF THE TREE IS IN ORDEN OR NOT----
 listOrd ::(Ord a) =>[a]->Bool
 listOrd [] = True
-listOrd [_] = True
+listOrd [x] = True
 listOrd ( x : y : xs ) = ( x <= y ) && listOrd ( y : xs )
 
 treeOrd :: (Ord a) =>BinTree a -> Bool
@@ -27,7 +27,29 @@ firstEle (x:xs) = (x,xs)
 
 -------------------------------------------------
 
+------- CONVERT THE LIST AT TREE ----------------
+listToTree::(Ord a) => [a] -> BinTree a
+listToTree []     = Null
+listToTree (x:xs) = insABB x (listToTree xs)
+
+-------------------------------------------------
 
 
---insABB:: (Ord a) => a -> BinTree a -> BinTree a--inserta un elemento en el arbol
+----- INSERT AN ELEMENT IN THE TREE -------------
+insABB::(Ord a) => a -> BinTree a -> BinTree a
+insABB x Null             = (Node Null x Null)
+insABB x (Node hi y hd) | x==y = (Node hi y hd)
+                        | x<y  = insABB x hi
+                        | x>y  = insABB x hd
+
+-------------------------------------------------
+
+
+
+{-nse :: (Ord a) => a -> BinTree a -> BinTree a
+inse x Null = (Node Null x Null)
+--inse x (Node i r d ) = (Node i r (Node Null x Null)) 
+inse x (Node i r d ) = inse x d
+-}
+
 --getMin :: (Ord a) => BinTree a -> (a, BinTree a) -- a = elemento mas chico y bintree a = arbol sin el elemento menor
